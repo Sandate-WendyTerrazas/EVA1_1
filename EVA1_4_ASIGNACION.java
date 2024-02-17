521b0b15022528401f5f0cbc6643547a45db380e
Sandate-WendyTerrazas <159495003+Sandate-WendyTerrazas@users.noreply.github.com>
2024-02-16 18:41:26 -0600

    Add files via upload

---
+++ EVA1_4_ASIGNACION.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_4_asignacion;
+
+/**
+ *
+ * @author wendy
+ */
+public class EVA1_4_ASIGNACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad;// DECLARACIÓN
+        edad = 18; //ASIGNACIÓN --> UN SOLO = SIGNIFICAR ASIGNAR
+        System.out.println("Tu edad es");
+        System.out.println(edad);
+        String nombre;// DECLARACIÓN
+        nombre = "wendy_sandate_terrazas";// ASIGNACION
+        System.out.println("Tu nombre es");
+        System.out.println(nombre);
+        String carrera;// DECLARACIÓN
+        carrera = "Ssistemas_computacionales";// ASIGNACIÓN
+        System.out.println("tu carrera es");
+        System.out.println(carrera);
+    }
+    
+}
